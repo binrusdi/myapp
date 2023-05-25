5e265ebda0b155f3372b3ff000fa1efb0be7d9c1
binrusdi <mrusdiana07@gmail.com>
2023-05-25 21:45:08 +0700

    Updates

---
+++ precache-manifest.a78f006126359af97d1509174b2da7de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "31ff0f3f712401c80fbd",
+    "url": "/myapp/static/css/main.2646bff0.chunk.css"
+  },
+  {
+    "revision": "31ff0f3f712401c80fbd",
+    "url": "/myapp/static/js/main.31ff0f3f.chunk.js"
+  },
+  {
+    "revision": "5a636ebf5bd42d219eff",
+    "url": "/myapp/static/js/1.5a636ebf.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "b88bfb2fa52194f7977eada4d4b3e325",
+    "url": "/myapp/index.html"
+  }
+];
